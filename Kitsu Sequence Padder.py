fb52f8b3fdb4b547f0247be0c357bf19579df13e
Jacob Danell <jacob@emberlight.se>
2022-04-20 16:44:21 +0200

    Added URL and Login checks

--- Kitsu Sequence Padder.py
+++ Kitsu Sequence Padder.py
@@ -1,4 +1,3 @@
-from pydoc import doc
 import gazu
 import os
 from dotenv import load_dotenv
@@ -11,21 +10,37 @@ PASSWORD = os.getenv('PASSWORD')
 PROJECT = os.getenv('PROJECT')
 PADDING = int(os.getenv('PADDING'))
 
-gazu.set_host(HOST + "/api")
-gazu.log_in(USER, PASSWORD)
 
-project_dict = gazu.project.get_project_by_name(PROJECT)
+def run():
 
-sequences_dict = gazu.shot.all_sequences_for_project(project_dict)
+    gazu.set_host(HOST + "/api")
+    if not gazu.client.host_is_up():
+        print("Can't find host " + HOST)
+        return
 
-for sequence in sequences_dict:
-    if len(sequence['name']) < PADDING:
-        old_name = sequence['name']
-        while len(sequence['name']) < PADDING:
-            sequence['name'] = '0' + sequence['name']
-        sucess = gazu.shot.update_sequence(sequence)
-        if sucess:
-            print(old_name + ' was renamed to ' + sequence['name'])
-        else:
-            print('Something went wrong when renaming ' +
-                  old_name + " to " + sequence['name'])
+    try:
+        gazu.log_in(USER, PASSWORD)
+    except Exception as exc:
+        print("Can't login. Please check your credentials in the .env file")
+        return
+
+    project_dict = gazu.project.get_project_by_name(PROJECT)
+
+    sequences_dict = gazu.shot.all_sequences_for_project(project_dict)
+
+    for sequence in sequences_dict:
+        if len(sequence['name']) < PADDING:
+            old_name = sequence['name']
+            while len(sequence['name']) < PADDING:
+                sequence['name'] = '0' + sequence['name']
+            sucess = gazu.shot.update_sequence(sequence)
+            if sucess:
+                print(old_name + ' was renamed to ' + sequence['name'])
+            else:
+                print('Something went wrong when renaming ' +
+                      old_name + " to " + sequence['name'])
+
+    print("Done!")
+
+
+run()
